824abeed4e6cc5a80e10488b36c469c380e6dc2c
Kelly Ryan <kellyryan.dev@gmail.com>
2025-04-22 19:47:48 +0100

    Updating dependencies.

--- src/services/bookAPI.ts
+++ src/services/bookAPI.ts
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from "axios";
 export const getBooks = async (): Promise<AxiosResponse> => {
   const response = await axios
     .get(
-      `https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=AIzaSyAXgulCS7V54aMe_KqML2VYSaG81Xf5ayI`,
+      `https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=${GOOGLE_BOOKS_API_KEY}`,
     )
     .then((response) => {
       console.log(response.data);
